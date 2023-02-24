ff2d78d1029fa09366d091c51eae6973d5ff602e
Arman-Hossen <arman15-12726@diu.edu.bd>
2023-02-24 18:11:19 +0600

    simple signupt

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD85M0hJEgnEcnsMVScPxrK7mWOxn4hSS8",
+  authDomain: "auth-router-context-1741c.firebaseapp.com",
+  projectId: "auth-router-context-1741c",
+  storageBucket: "auth-router-context-1741c.appspot.com",
+  messagingSenderId: "322691669207",
+  appId: "1:322691669207:web:0b967bfaec6788661ea492"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
